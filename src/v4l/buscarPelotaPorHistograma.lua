5d317c66b03fdc4348810536d0810544386fbc1c
Rafael Sisto <rafael.sisto@gmail.com>
2012-07-11 18:34:28 -0300

    algunas pruebas de histograma

--- src/v4l/buscarPelotaPorHistograma.lua
+++ src/v4l/buscarPelotaPorHistograma.lua
@@ -11,26 +11,36 @@ colorRef={
 
 
 
-img,w,h = ppm2mem("image2.ppm")
+local img,w,h = ppm2mem("image2.ppm")
 
-histBall={}
 
-for x=0,w-1 do 
-	histBall[x+1] =0
-	for y=0,h-1 do
-		local i=y*w+x
-		local r,g,b = img[i*3+1],img[i*3 +2] ,img[i*3 +3]
-		print("x=" .. x .." y="..y.. " " ..r  .. " " ..g .." " .. b ) 
-		local hue = rgb2hue(r,g,b)
-		local isBall = hueDiff(hue , colorRef.ball[1]) <= colorRef.ball[2]
-		if isBall and x == 144 then
-			print("rgb 144" ..r .." " .. g .. " " .. b .. " hue: " .. hue .. " y  " ..y)
-			
+--Returns the historgram for the ball and posts color
+function getHistogramForBallAndPosts(img,w,h,colorRef)
+
+	histBall={}
+	histPosts = {}
+	
+	for x=0,w-1 do 
+		histPosts[x+1] =0
+		histBall[x+1] =0
+		for y=0,h-1 do
+			local i=y*w+x
+			local r,g,b = img[i*3+1],img[i*3 +2] ,img[i*3 +3]
+			print("x=" .. x .." y="..y.. " " ..r  .. " " ..g .." " .. b ) 
+			local hue = rgb2hue(r,g,b)
+			--Search for ball only in the lower third of the screen
+			if y > (2*h)/3 and hueDiff(hue , colorRef.ball[1]) <= colorRef.ball[2] then
+				histBall[x+1] = histBall[x+1] +1 
+			end
+			if hueDiff(hue , colorRef.post[1]) <= colorRef.post[2] then 
+				histPost[x+1] = histPost[x+1] +1
+			end 
 		end
-		if isBall then histBall[x+1] = histBall[x+1] +1 end
 	end
-	print("hball "..x .." " .. histBall[x+1])
+	return histBall, histPosts
 end
+
+histBall = getHistogramForBallAndPosts(img,w,h,colorRef)
 
 xBall,maxHistBall = -1 , 0
 for x=1,w do
